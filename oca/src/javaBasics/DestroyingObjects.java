3cc6b9d0e9a759d3eff2bc57e80b429b58a35cd0
rafaelbernabeu <rbbernabeu@gmail.com>
2018-06-30 11:58:31 -0300

    update on gc example.

--- oca/src/javaBasics/DestroyingObjects.java
+++ oca/src/javaBasics/DestroyingObjects.java
@@ -2,16 +2,23 @@ package javaBasics;
 
 public class DestroyingObjects {
 
-    public static void main(String[] args) {
-        Object obj = new Object();
-        obj = null;
+    private static int objFinalized = 0;
 
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 1000; i++) {
+            DestroyingObjects obj = new DestroyingObjects();
+            obj = new DestroyingObjects();
+        }
         System.gc(); // Java is free to ignore, is not guaranteed to run.
+        Thread.sleep(1000L); //time to print the number of finalized objects
     }
 
+    /**
+     * might not get called and definitely won't be called twice
+     */
     @Override
     protected void finalize() {
-        System.out.println("might not get called and definitely won't be called twice");
+        System.out.println(++objFinalized);
     }
 
 }
